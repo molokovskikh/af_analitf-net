827fcd155969c320a010c7459cbb88844f7ceb35
Квасов Роман <r.kvasov@analit.net>
2012-12-20 12:20:24 +0400

    Добавил отображение примечания на панели с информацией о предложении

--- scripts/run-mysql-client.sh
+++ scripts/run-mysql-client.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 path=src/AnalitF.Net.Client/bin/run
 path=$(cygpath -aw $path)
+mysqldb=$(dirname $(dirname $(which mysqld)))/data/mysql
+cp $mysqldb $path -r
 mysqld --console --standalone --port=3310 --datadir="${path/\\/\//}"&
+mysql --user=root --port=3310
