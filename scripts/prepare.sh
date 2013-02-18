7db9ebcb2c95565eaea66821cf70c12800568e86
Квасов Роман <r.kvasov@analit.net>
2013-02-18 16:12:07 +0400

    Исправил сборку

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -15,3 +15,4 @@ cp lib/libmysqld/* src/AnalitF.Net.Test/bin/debug/ -r
 cp lib/libmysqld/* src/AnalitF.Net.Client/bin/debug/ -r
 cp lib/libmysqld/* src/AnalitF.Net.Client/bin/run/ -r
 cp assets/2block.gif src/data/ads/Воронеж_1/
+cp assets/Common.Models.Tests.config src/Common.Models/Common.Models.Tests/App.config
