525f6fd4b300f1b3263f51d5ba6c475686299830
Квасов Роман <r.kvasov@analit.net>
2013-04-12 09:31:56 +0400

    исправил скрипт

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -3,4 +3,7 @@
 lastTag=`git tag | tail -n1`
 git log $lastTag...head > output/changes.txt
 newTag=b$[${lastTag/b/}+1]
+
+/bin/egrep "#[0-9]+" output/changes.txt > output/issues.txt
+
 git tag $newTag head
