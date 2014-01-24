cc5d02395b2570891c95e5e4a84370eab9e514d1
Квасов Роман <r.kvasov@analit.net>
2014-01-24 12:30:18 +0400

    Исправил сборку

--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-vboxmanage guestcontrol win7 copyto `cygpath -wa run.cmd` 'C:\users\test\' --username=test --password=123
-vboxmanage guestcontrol win7 copyto `cygpath -wa elevate.exe` 'C:\users\test\' --username=test --password=123
-vboxmanage guestcontrol win7 exec 'C:\users\test\run.cmd' --username=test --password=123 --wait-exit --wait-stdout --wait-stderr | iconv -f cp866 -t utf-8
--- scripts/vm-install.sh
+++
@@ -1,5 +0,0 @@
-pass=12345678
-bake build:client env=test
-vboxmanage guestcontrol win7 copyto `cygpath -wa output/setup/setup.exe` 'C:\users\test\' --username test --password $pass
-vboxmanage guestcontrol win7 exec --verbose --timeout 1800000 --image 'C:\users\test\setup.exe' --username test --password $pass --wait-exit --wait-stdout -- -q | iconv -f cp1251 -t utf-8
-vboxmanage guestcontrol win7 exec --verbose --image 'C:\users\test\appdata\local\аналитфармация\analitf.net.client.exe' --username test --password $pass --wait-exit --wait-stdout -- --quiet start-check | iconv -f cp1251 -t utf-8
