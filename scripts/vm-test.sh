9f6cd05b717c668adb7678b68f3e988007330345
Квасов Роман <r.kvasov@analit.net>
2014-03-06 16:57:06 +0400

    Включил приемочные тесты

--- scripts/vm-test.sh
+++ scripts/vm-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#nunit-console-x86 /nologo /run AnalitF.Net.Client.Test.Acceptance.UpdateFixture,AnalitF.Net.Client.Test.Acceptance.StartFixture $* ./src/AnalitF.Net.Test/bin/Debug/AnalitF.Net.Client.Test.dll
+nunit-console-x86 /nologo /run AnalitF.Net.Client.Test.Acceptance.UpdateFixture,AnalitF.Net.Client.Test.Acceptance.StartFixture $* ./src/AnalitF.Net.Test/bin/Debug/AnalitF.Net.Client.Test.dll
 
 rm ./src/client/test/vm/bin/Debug/setup.exe
 nunit-console /nologo /run vm.VMFixture $* ./src/client/test/vm/bin/Debug/vm.dll
