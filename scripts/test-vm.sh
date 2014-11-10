9d89e277a79a0d8154e94edb6ec55dddc3e373ed
Квасов Роман <r.kvasov@analit.net>
2014-11-10 18:24:22 +0400

    Исправил скрипт для запуска тестов

--- scripts/test-vm.sh
+++ scripts/test-vm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-nunit-console-x86 /nologo /run AnalitF.Net.Client.Test.Acceptance.UpdateFixture,AnalitF.Net.Client.Test.Acceptance.StartFixture $* ./src/client/test/bin/Debug/AnalitF.Net.Client.Test.dll
+set -o errexit
+nunit_opts="/nologo /stoponerror /labels"
+nunit-console-x86 $nunit_opts /run AnalitF.Net.Client.Test.Acceptance.UpdateFixture,AnalitF.Net.Client.Test.Acceptance.StartFixture $* ./src/client/test/bin/Debug/AnalitF.Net.Client.Test.dll
 
-rm ./src/client/test/vm/bin/Debug/setup.exe
-nunit-console /nologo /run vm.VMFixture $* ./src/client/test/vm/bin/Debug/vm.dll
+rm ./src/client/test/vm/bin/Debug/setup.exe || :
+nunit-console $nunit_opts /run vm.VMFixture $* ./src/client/test/vm/bin/Debug/vm.dll
