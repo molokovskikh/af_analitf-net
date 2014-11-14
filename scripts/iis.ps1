889190bca619e540e64ed2e3b2e15d11312f97ae
Квасов Роман <r.kvasov@analit.net>
2014-11-14 10:20:32 +0300

    Исправил развертывание приложений

--- scripts/iis.ps1
+++ scripts/iis.ps1
@@ -1,7 +1,8 @@
 $app = $args[0]
 $site = $args[1]
 $path = $args[2]
-New-WebApplication -Site $site $app -PhysicalPath $path
+$pool = $args[3]
+New-WebApplication -Site $site $app -PhysicalPath $path -ApplicationPool $pool
 Set-WebConfiguration system.webServer/security/authentication/anonymousAuthentication -PSPath IIS:\ -Location $site/$app -Value @{enabled="False"}
 Set-WebConfiguration system.webServer/security/authentication/basicAuthentication -PSPath IIS:\ -Location $site/$app -Value @{enabled="True"}
 Set-WebConfiguration system.webServer/security/authentication/basicAuthentication -PSPath IIS:\ -Location $site/$app -Value @{defaultLogonDomain="adc.analit.net"}
