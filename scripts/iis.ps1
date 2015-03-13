3282bacfd9cd27e6c6cd45534205c40abcd7d430
Квасов Роман <r.kvasov@analit.net>
2015-03-13 16:54:36 +0300

    Сборка: исправил регистрацию приложений в iis

--- scripts/iis.ps1
+++ scripts/iis.ps1
@@ -1,7 +1,12 @@
-$app = $args[0]
+﻿$app = $args[0]
 $site = $args[1]
 $path = $args[2]
 $pool = $args[3]
+$existApp = Get-WebApplication -Site $site $app
+if ($existApp -ne $null) {
+	echo "Приложение $site/$app уже существует"
+	exit
+}
 New-WebApplication -Site $site $app -PhysicalPath $path -ApplicationPool $pool
 Set-WebConfiguration system.webServer/security/authentication/anonymousAuthentication -PSPath IIS:\ -Location $site/$app -Value @{enabled="False"}
 Set-WebConfiguration system.webServer/security/authentication/basicAuthentication -PSPath IIS:\ -Location $site/$app -Value @{enabled="True"}
