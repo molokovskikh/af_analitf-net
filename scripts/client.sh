55bade2e38681978a0ca097ce02a6ef862d8c126
Квасов Роман <r.kvasov@analit.net>
2015-06-11 10:56:25 +0300

    Исправил тестовый запуск приложения

--- scripts/client.sh
+++ scripts/client.sh
@@ -2,4 +2,4 @@
 
 run=`cygpath -aw src/client/app/bin/run/`
 bin=`cygpath -aw src/client/app/bin/debug/AnalitF.Net.Client.exe`
-bin-guard --no-stdin $run $bin
+bin-guard --no-stdin $run $bin | iconv --from cp866 --to utf-8
