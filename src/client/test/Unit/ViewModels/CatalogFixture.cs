ae1e9de980dd96f2c356508c968c1a152c99e4dd
Квасов Роман <r.kvasov@analit.net>
2015-07-21 15:58:44 +0300

    Ошибка #36715
    Замечания по версии 77:
    Не отображаются синонимы (при поиске по части наименовани)

--- src/client/test/Unit/ViewModels/CatalogFixture.cs
+++ src/client/test/Unit/ViewModels/CatalogFixture.cs
@@ -1,6 +1,7 @@
 ﻿using AnalitF.Net.Client.Models;
 using AnalitF.Net.Client.Test.TestHelpers;
 using AnalitF.Net.Client.ViewModels;
+using AnalitF.Net.Test.Integration.ViewModes;
 using NUnit.Framework;
 
 namespace AnalitF.Net.Test.Unit.ViewModels
@@ -19,6 +20,25 @@ namespace AnalitF.Net.Test.Unit.ViewModels
 			model = new CatalogViewModel();
 			Activate(model);
 			Assert.True(model.CanExport);
+		}
+
+		[Test]
+		public void Reset_term_on_show_with_same_mnn()
+		{
+			BaseScreen.TestQueryScheduler = new CurrentThreadTaskScheduler();
+			var model = new CatalogViewModel();
+			Activate(model);
+			model.CatalogSearch.Value = true;
+			model.SearchText = "тест";
+			var search = ((CatalogSearchViewModel)model.ActiveItem);
+			search.SearchBehavior.Search();
+			search.CurrentCatalog.Value = new Catalog("тест") {
+				Name = {
+					Mnn = new Mnn()
+				}
+			};
+			model.FilterByMnn = true;
+			Assert.AreEqual("", search.SearchBehavior.ActiveSearchTerm.Value);
 		}
 	}
 }
