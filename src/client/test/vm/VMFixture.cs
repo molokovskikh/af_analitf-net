79df54572995cc98698c3ad1da16a0f0a5a55915
Квасов Роман <r.kvasov@analit.net>
2015-09-17 18:11:18 +0300

    Исправил тесты

--- src/client/test/vm/VMFixture.cs
+++ src/client/test/vm/VMFixture.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Common.Tools;
+using Common.Tools.Calendar;
 using Common.Tools.Helpers;
 using NUnit.Framework;
 using VirtualBox;
@@ -14,8 +15,8 @@ namespace vm
 	[TestFixture, Explicit, Category("VM")]
 	public class VMFixture
 	{
-		string user = "IEUser";
-		string password = "12345678";
+		string user = "test";
+		string password = "123";
 		string root;
 		string setup;
 		bool forceShutdown = true;
@@ -27,6 +28,7 @@ namespace vm
 
 		//string guiType = "gui";
 		string guiType = "headless";
+		string vmName = "winxp";
 
 		public VMFixture()
 		{
@@ -39,8 +41,8 @@ namespace vm
 		{
 			//кэшируем файл что бы каждый раз не собирать
 			if (!File.Exists("setup.exe")) {
-				var cmd = String.Format("bash.exe -l -c \"cd `cygpath '{0}'`;{1}\"", root, "bake build:client env=test");
-				ProcessHelper.CmdDir(cmd, root);
+				var cmd = $"C:\\cygwin\\bin\\bash.exe -l -c \"cd `cygpath '{root}'`;bake build:client env=test\"";
+				ProcessHelper.CmdDir(cmd, root, timeout: 5.Minute());
 				File.Copy(setup, "setup.exe");
 			}
 
@@ -54,26 +56,47 @@ namespace vm
 		}
 
 		[Test]
-		public void Install_win7()
+		public void Install()
 		{
 			var guestsession = Boot();
 
-			var target = String.Format(@"C:\users\{0}\setup.exe", user);
+			var guestRoot = $@"C:\users\{user}";
+			var installRoot = Path.Combine(guestRoot, @"appdata\local\аналитфармация\");
+			if (machine.OSTypeId == "WindowsXP") {
+				guestRoot = $@"C:\Documents and Settings\{user}";
+				installRoot = Path.Combine(guestRoot, @"Local Settings\Application Data\АналитФАРМАЦИЯ");
+			}
+
+			var target = Path.Combine(guestRoot, "setup.exe");
 			var copyTo = guestsession.CopyTo(setup, target, null);
 			copyTo.WaitForCompletion(-1);
-			Assert.IsNull(copyTo.ErrorInfo, copyTo.ErrorInfo != null ? copyTo.ErrorInfo.Text : "");
+			Assert.IsNull(copyTo.ErrorInfo, copyTo.ErrorInfo?.Text);
 
 			Start(guestsession, target, "/quiet");
-			var exe = String.Format(@"C:\users\{0}\appdata\local\аналитфармация\AnalitF.Net.Client.exe", user);
+			var exe = Path.Combine(installRoot, "AnalitF.Net.Client.exe");
 			Start(guestsession, exe, "--quiet", "start-check");
 
-			var log = String.Format(@"C:\users\{0}\appdata\local\аналитфармация\AnalitF.Net.Client.log", user);
+			var log = Path.Combine(installRoot, "AnalitF.Net.Client.log");
 			File.Delete("log.txt");
 			var copyLog = guestsession.CopyFrom(log, Path.GetFullPath("log.txt"), null);
 			copyLog.WaitForCompletion(-1);
-			Assert.IsNull(copyLog.ErrorInfo, copyLog.ErrorInfo != null ? copyLog.ErrorInfo.Text : "");
+			Assert.IsNull(copyLog.ErrorInfo, copyLog.ErrorInfo?.Text);
 			var logText = File.ReadAllText("log.txt");
-			Assert.AreEqual("", logText);
+			Assert.AreEqual(logText, Is.Not.StringContaining("ERROR"));
+		}
+
+		[Test, Ignore]
+		public void Snap()
+		{
+			machine.LockMachine(session, LockType.LockType_Write);
+
+			if (machine.SnapshotCount > 0) {
+				var snapshot = machine.FindSnapshot("origin");
+				session.Console.DeleteSnapshot(snapshot.Id).WaitForCompletion(-1);
+			}
+
+			session.Console.TakeSnapshot("origin", "чистое состояние").WaitForCompletion(-1);
+			session.UnlockMachine();
 		}
 
 		private static void Start(IGuestSession guestsession, string target, params string[] arguments)
@@ -87,7 +110,7 @@ namespace vm
 				|| setupProcess.Status == ProcessStatus.ProcessStatus_Starting) {
 				Thread.Sleep(1000);
 			}
-			Assert.AreEqual(setupProcess.Status, ProcessStatus.ProcessStatus_TerminatedNormally, cmd);
+			Assert.AreEqual(ProcessStatus.ProcessStatus_TerminatedNormally, setupProcess.Status, cmd);
 		}
 
 		private IGuestSession Boot()
@@ -110,14 +133,18 @@ namespace vm
 		private IGuestSession WaitBoot()
 		{
 			var console = session.Console;
-			WaitState(machine, MachineState.MachineState_Running);
 			var guestsession = StartGuest(console);
 
 			var done = false;
+			var i = 0;
 			while (!done) {
 				try {
+					i++;
+					if (i > 300)
+						throw new Exception("Не удалось дождать запуска виртуальной машины за 5 минут," +
+							$" проверь имя пользователя и пароль для входа, пытаюсь войти как {user} {password}");
 					Thread.Sleep(1000);
-					var echoTest = guestsession.ProcessCreate("cmd.exe", new[] { "/c", "echo" }, null, null, 0);
+					var echoTest = guestsession.ProcessCreate("C:\\windows\\system32\\cmd.exe", new[] { "/c", "echo", "hello!" }, null, null, 0);
 					WaitForExit(echoTest);
 					done = echoTest.Status == ProcessStatus.ProcessStatus_TerminatedNormally;
 				}
@@ -141,7 +168,7 @@ namespace vm
 		{
 			vm = new VirtualBoxClass();
 			session = new SessionClass();
-			machine = vm.Machines.Cast<IMachine>().First(i => i.Name == "win7");
+			machine = vm.Machines.Cast<IMachine>().First(i => i.Name == vmName);
 		}
 
 		private void Start()
@@ -166,9 +193,9 @@ namespace vm
 		private IGuestSession StartGuest(IConsole console)
 		{
 			var guestsession = console.Guest.CreateSession(user, password, "", "test");
-			while (guestsession.Status != GuestSessionStatus.GuestSessionStatus_Started) {
-				Thread.Sleep(1000);
-			}
+			WaitHelper.WaitOrFail(10.Second(),
+				() => guestsession.Status == GuestSessionStatus.GuestSessionStatus_Started,
+				$"состояние сессии {guestsession.Status} ждем GuestSessionStatus.GuestSessionStatus_Started");
 			return guestsession;
 		}
 
@@ -184,42 +211,42 @@ namespace vm
 			catch (Exception) {}
 
 			machine.LockMachine(session, LockType.LockType_Shared);
-			var guestsession = StartGuest(session.Console);
-			var shutdown = guestsession.ProcessCreate("cmd.exe", new[] {"/C", "shutdown", "/p"}, null, null, 0);
-			Assert.That(shutdown.Status, Is.EqualTo(ProcessStatus.ProcessStatus_Starting)
-				.Or.EqualTo(ProcessStatus.ProcessStatus_Started));
-			session.UnlockMachine();
+			try {
+				session.Console.PowerButton();
+				//var guestsession = StartGuest(session.Console);
+				//var shutdown = guestsession.ProcessCreate("cmd.exe", new[] {"/C", "shutdown", "/p"}, null, null, 0);
+				//Assert.That(shutdown.Status, Is.EqualTo(ProcessStatus.ProcessStatus_Starting)
+				//	.Or.EqualTo(ProcessStatus.ProcessStatus_Started));
+			}
+			finally {
+				session.UnlockMachine();
+			}
 
 			WaitState(machine, MachineState.MachineState_PoweredOff);
 		}
 
 		private static void WaitState(IMachine machine, MachineState state)
 		{
-			while (machine.State != state) {
-				Thread.Sleep(1000);
-			}
-		}
-
-		[Test, Ignore]
-		public void Snap()
-		{
-			machine.LockMachine(session, LockType.LockType_Write);
-
-			if (machine.SnapshotCount > 0) {
-				var snapshot = machine.FindSnapshot("origin");
-				session.Console.DeleteSnapshot(snapshot.Id).WaitForCompletion(-1);
-			}
-
-			session.Console.TakeSnapshot("origin", "чистое состояние").WaitForCompletion(-1);
-			session.UnlockMachine();
+			WaitHelper.WaitOrFail(30.Second(),
+				() => machine.State == state,
+				$"состояния машины {state} текущеее состояние {machine.State}");
 		}
 
 		private void Restore()
 		{
 			machine.LockMachine(session, LockType.LockType_Write);
-			var snapshot = machine.FindSnapshot("origin");
-			session.Console.RestoreSnapshot(snapshot).WaitForCompletion(-1);
-			session.UnlockMachine();
+			try {
+				if (machine.SnapshotCount == 0) {
+					session.Console.TakeSnapshot("origin", "чистое состояние").WaitForCompletion(-1);
+				}
+				else {
+					var snapshot = machine.FindSnapshot("origin");
+					session.Console.RestoreSnapshot(snapshot).WaitForCompletion(-1);
+				}
+			}
+			finally {
+				session.UnlockMachine();
+			}
 		}
 	}
 }
