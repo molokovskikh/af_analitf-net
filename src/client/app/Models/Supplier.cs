ec28d93a6b61297a00208a94d0ba58ed73088ca9
Квасов Роман <r.kvasov@analit.net>
2015-09-17 13:58:27 +0300

    Исправил сборку

--- src/client/app/Models/Supplier.cs
+++ src/client/app/Models/Supplier.cs
@@ -12,15 +12,5 @@ namespace AnalitF.Net.Client.Models
 		public virtual string FullName { get; set; }
 
 		public virtual bool HaveCertificates { get; set; }
-
-		public static void InvalidateCache(TestSupplier supplier, User user, ISession session)
-		{
-			session.CreateSQLQuery(@"update Usersettings.AnalitFReplicationInfo
-set ForceReplication = 1
-where userId = :userId and FirmCode = :supplierId")
-				.SetParameter("supplierId", supplier.Id)
-				.SetParameter("userId", user.Id)
-				.ExecuteUpdate();
-		}
 	}
 }
