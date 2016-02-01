61c851593f720f81d15c220b4288b1f1535442c2
Квасов Роман <r.kvasov@analit.net>
2016-02-01 16:08:26 +0300

    Исправил сборку

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,11 +9,11 @@ mkdir src/client/test/bin/Debug/var/service || :
 mkdir src/client/test/bin/Debug/var/service/localexport || :
 
 chmod o+w src/client/test/bin/Debug/var/service/localexport
-nunit-console-x86 /nologo /stoponerror /labels $* ./src/client/test/bin/Debug/AnalitF.Net.Client.Test.dll
+nunit3-console --labels=all --stoponerror --verbose --full $* ./src/client/test/bin/Debug/AnalitF.Net.Client.Test.dll
 mv TestResult.xml output/test/AnalitF.Net.Client.Test.dll.xml
 
 mkdir src/service/test/bin/Debug/var || :
 mkdir src/service/test/bin/Debug/var/localexport || :
 chmod o+w src/service/test/bin/Debug/var/localexport
-nunit-console-x86 /nologo /stoponerror /labels $* ./src/service/test/bin/Debug/AnalitF.Net.Service.Test.dll
+nunit3-console /nologo /stoponerror /labels $* ./src/service/test/bin/Debug/AnalitF.Net.Service.Test.dll
 mv TestResult.xml output/test/AnalitF.Net.Service.Test.dll.xml
