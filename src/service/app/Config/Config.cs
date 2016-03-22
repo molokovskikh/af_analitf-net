b380d407b3f1500961feaba6fd28b0f9201f2fe5
Квасов Роман <r.kvasov@analit.net>
2016-03-22 09:54:12 +0300

    Включил автоматическое определение канала обновления

--- src/service/app/Config/Config.cs
+++ src/service/app/Config/Config.cs
@@ -51,17 +51,16 @@ namespace AnalitF.Net.Service.Config
 
 		public string GetUpdatePath(AnalitfNetData data, RequestLog job)
 		{
-			//клиентское приложение передает некорректную информацию
-			//var channel = data.BinUpdateChannel?.ToLower();
-			//if (channel?.StartsWith("migration") == true) {
-			//	channel = channel.ToLower().Replace("migration-", "").Replace("migration", "");
-			//}
-			//if (job.Branch == "master")
-			//	return Path.Combine(UpdatePath,
-			//		String.IsNullOrEmpty(channel) ? "rtm" : channel);
-			//else if (job.Branch == "migration")
-			//	return Path.Combine(UpdatePath,
-			//		String.IsNullOrEmpty(channel) || channel == "rtm" ? "migration" : "migration-" + channel);
+			var channel = data.BinUpdateChannel?.ToLower();
+			if (channel?.StartsWith("migration") == true) {
+				channel = channel.ToLower().Replace("migration-", "").Replace("migration", "");
+			}
+			if (job.Branch == "master")
+				return Path.Combine(UpdatePath,
+					String.IsNullOrEmpty(channel) ? "rtm" : channel);
+			else if (job.Branch == "migration")
+				return Path.Combine(UpdatePath,
+					String.IsNullOrEmpty(channel) || channel == "rtm" ? "migration" : "migration-" + channel);
 
 			return Path.Combine(UpdatePath,
 				String.IsNullOrEmpty(data.BinUpdateChannel) ? "rtm" : data.BinUpdateChannel);
