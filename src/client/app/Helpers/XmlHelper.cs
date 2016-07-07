9c32d51c3cf0babf17153be26d5669c9d297bab0
Михаил Шунько <mihail.shunko@yandex.ru>
2016-07-07 15:42:49 +0300

    Парсинг xml

---
+++ src/client/app/Helpers/XmlHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Xml.XPath;
+
+namespace AnalitF.Net.Client.Helpers
+{
+	public class XMLDocHelper
+	{
+		public XMLDocHelper(byte[] xmldocument)
+		{
+			var str = Encoding.Default.GetString(xmldocument);
+			var streader = new StringReader(str);
+			xmlDocument = new XPathDocument(streader);
+			xNavigator = xmlDocument.CreateNavigator();
+		}
+
+		public XMLDocHelper(string xmldocument)
+		{
+			var streader = new StringReader(xmldocument);
+			xmlDocument = new XPathDocument(streader);
+			xNavigator = xmlDocument.CreateNavigator();
+		}
+
+		public string GetValue(string xpath)
+		{
+			var node = xNavigator.Select(xpath);
+			node.MoveNext();
+			return node.Current.Value;
+		}
+
+		protected XPathDocument xmlDocument;
+		protected XPathNavigator xNavigator;
+	}
+}
