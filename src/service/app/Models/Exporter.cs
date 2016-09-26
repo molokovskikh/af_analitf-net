cdd670de202d4031a9e0eed68a1f272032265cd3
Пётр Шаламов <nekkuro1@gmail.com>
2016-09-26 14:56:51 +0500

    49877: временное изменение экспортера

--- src/service/app/Models/Exporter.cs
+++ src/service/app/Models/Exporter.cs
@@ -1572,6 +1572,7 @@ where a.MailId in ({ids.Implode()})";
 
 			var addresses = Addresses.Select(a => (uint?)a.Id).ToArray();
 			var prices = session.Query<ActivePrice>().Select(p => p.Id).ToArray();
+			//MessageBox.Show(session.Query<Order>().ToArray().Length.ToString());
 			var orders = session.Query<Order>()
 				.Where(o => !o.Deleted
 					&& !o.Processed
@@ -1579,8 +1580,11 @@ where a.MailId in ({ids.Implode()})";
 					//&& o.UserId != user.Id
 					&& addresses.Contains(o.AddressId))
 				.ToArray();
+			//MessageBox.Show(orders.Length.ToString());
 			orders = orders.Where(o => prices.Contains(new PriceKey(o.PriceList, o.RegionCode))).ToArray();
+			//MessageBox.Show(orders.Length.ToString());
 			var groups = orders.GroupBy(o => new { o.AddressId, o.PriceList, o.RegionCode });
+			//MessageBox.Show(groups.ToArray().Length.ToString());
 			foreach (var @group in groups) {
 				foreach (var order in group) {
 					session.Save(new PendingOrderLog(order, user, group.First().RowId));
