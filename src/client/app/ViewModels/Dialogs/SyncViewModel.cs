1b2164df54c02b04127e6441380623ddb0231147
Квасов Роман <r.kvasov@analit.net>
2016-10-14 10:51:57 +0300

    Исправил тест

--- src/client/app/ViewModels/Dialogs/SyncViewModel.cs
+++ src/client/app/ViewModels/Dialogs/SyncViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using AnalitF.Net.Client.Config;
 using AnalitF.Net.Client.Helpers;
@@ -9,17 +10,25 @@ namespace AnalitF.Net.Client.ViewModels.Dialogs
 {
 	public class SyncViewModel : WaitViewModel
 	{
+		private CancellationDisposable disposable = new CancellationDisposable();
+
 		public SyncViewModel(IObservable<Progress> progress, IScheduler scheduler)
 		{
 			Text = "Производится обмен данными.\r\nПожалуйста подождите.";
 			DisplayName = "Обмен данными";
 			Time = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1.0), scheduler)
 				.Select(v => TimeSpan.FromSeconds(v))
-				.ToValue();
-			Progress = progress.Sample(TimeSpan.FromMilliseconds(300), Env.Current.UiScheduler).ToValue();
+				.ToValue(disposable);
+			Progress = progress.Sample(TimeSpan.FromMilliseconds(300), Env.Current.UiScheduler).ToValue(disposable);
 		}
 
 		public NotifyValue<TimeSpan> Time { get; set; }
 		public NotifyValue<Progress> Progress { get; set; }
+
+		public override void TryClose()
+		{
+			base.TryClose();
+			disposable.Dispose();
+		}
 	}
 }
