8c17114aa8452d1dc393848e34179251395475a2
Пётр Шаламов <nekkuro1@gmail.com>
2016-10-18 12:49:51 +0500

    55795: создание сообщения

---
+++ src/client/test/Fixtures/CreateMessageUpdateInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using Test.Support;
+
+namespace AnalitF.Net.Client.Test.Fixtures
+{
+	public class CreateMessageUpdateInfo : ServerFixture
+	{
+		public override void Execute(ISession session)
+		{
+			var user = User(session);
+			user.UpdateInfo.Message = "Test Message";
+			user.UpdateInfo.MessageShowCount = 3;
+			session.Flush();
+		}
+	}
+}
