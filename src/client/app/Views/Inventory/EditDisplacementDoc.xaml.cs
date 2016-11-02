723e53c5c4bdf9a16c4de8a606eccb986c88ef9e
Пётр Шаламов <nekkuro1@gmail.com>
2016-11-02 12:50:20 +0500

    54193: поправлены кнопки на форме редактирования

--- src/client/app/Views/Inventory/EditDisplacementDoc.xaml.cs
+++ src/client/app/Views/Inventory/EditDisplacementDoc.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Caliburn.Micro;
 
 namespace AnalitF.Net.Client.Views.Inventory
 {
@@ -19,9 +20,30 @@ namespace AnalitF.Net.Client.Views.Inventory
 	/// </summary>
 	public partial class EditDisplacementDoc : UserControl
 	{
+		public ViewModels.Inventory.EditDisplacementDoc Model => DataContext as ViewModels.Inventory.EditDisplacementDoc;
+
 		public EditDisplacementDoc()
 		{
 			InitializeComponent();
+
+			Loaded += (sender, args) => {
+				Lines.Focus();
+			};
+
+			Keyboard.AddPreviewKeyDownHandler(this, (sender, args) => {
+				IEnumerable<IResult> results = null;
+				if (args.Key == Key.Insert) {
+					results = Model.AddLine();
+					args.Handled = true;
+				} else if (args.Key == Key.Delete) {
+					Model.DeleteLine();
+					args.Handled = true;
+				}
+				if (results != null) {
+					Coroutine.BeginExecute(results.GetEnumerator(), new ActionExecutionContext { View = this });
+					args.Handled = true;
+				}
+			});
 		}
 	}
 }
