d75700c16320144931092f2cbf88ded7209012f3
Ивар Лекса <i.lexa@analit.net>
2017-01-19 11:54:29 +0300

    Ошибка #58901 некорректное название фильтра

--- src/client/app/ViewModels/Inventory/Stocks.cs
+++ src/client/app/ViewModels/Inventory/Stocks.cs
@@ -76,6 +76,7 @@ namespace AnalitF.Net.Client.ViewModels.Inventory
 		{
 			base.OnInitialize();
 			AddressSelector.Init();
+			AddressSelector.Description = "Все накладные";
 
 			Bus.Listen<string>("reload").Cast<object>()
 				.Merge(DbReloadToken)
