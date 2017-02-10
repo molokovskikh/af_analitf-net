42e1e949c08ccd748d9565d997897986761c8630
Бандеев Даша-Нима <d.bandeev@analit.net>
2017-02-10 17:42:13 +0800

    57225 test

--- src/client/test/Integration/Commands/ImportFixture.cs
+++ src/client/test/Integration/Commands/ImportFixture.cs
@@ -21,7 +21,8 @@ namespace AnalitF.Net.Client.Test.Integration.Commands
 			var data = new List<Tuple<string, string[]>> {
 				Tuple.Create(TempFile("Users.txt", "5\ttest\t"), new[] { "Id", "NonExistsColumn" })
 			};
-			var cmd = InitCmd(new ImportCommand(data) {
+			var cmd = InitCmd(new ImportCommand(data)
+			{
 				Strict = false
 			});
 			cmd.Execute();
@@ -78,7 +79,7 @@ namespace AnalitF.Net.Client.Test.Integration.Commands
 					}
 					Assert.AreEqual("тестовый адрес доставки после ручного изменения", ws.Address);
 					// проводим загрузку
-					var data1 = new List<Tuple<string, string[]>> {Tuple.Create(TempFile("Users.txt", "5\ttest\t"), new[] { "Id", "NonExistsColumn" })};
+					var data1 = new List<Tuple<string, string[]>> { Tuple.Create(TempFile("Users.txt", "5\ttest\t"), new[] { "Id", "NonExistsColumn" }) };
 					using (var transaction = session.BeginTransaction())
 					{
 						var cmd = InitCmd(new ImportCommand(data1)
@@ -95,6 +96,52 @@ namespace AnalitF.Net.Client.Test.Integration.Commands
 			}
 
 
+		}
+
+		[Test]
+		public void NOChangeAddressImport_future_data()
+		{
+			Address address = session.Query<Address>().FirstOrDefault();
+			if (address != null)
+
+			{
+				WaybillSettings ws = session.Query<WaybillSettings>().FirstOrDefault(x => x.BelongsToAddress.Id == address.Id);
+				if (ws != null)
+				{
+					using (var transaction = session.BeginTransaction())
+					{
+						address.Name = "тестовый адрес доставки до изменения";
+						ws.Address = "тестовый адрес доставки до изменения";
+						session.Save(ws);
+						transaction.Commit();
+					}
+					Assert.AreEqual("тестовый адрес доставки до изменения", ws.Address);
+
+					using (var transaction = session.BeginTransaction())
+					{
+						ws.Address = "тестовый адрес доставки после ручного изменения";
+						session.Save(ws);
+						transaction.Commit();
+					}
+					ws = session.Query<WaybillSettings>().FirstOrDefault(x => x.BelongsToAddress.Id == address.Id);
+					Assert.AreEqual("тестовый адрес доставки после ручного изменения", ws.Address);
+
+					var data = new List<Tuple<string, string[]>> {
+						Tuple.Create(TempFile("Addresses.txt", address.Id.ToString() + "\tтестовый адрес доставки до изменения\t0\tЮридическое лицо\t", System.Text.Encoding.GetEncoding(1251)), new[] { "truncate", "Id", "Name", "HaveLimits", "Org"})};
+					using (var transaction = session.BeginTransaction())
+					{
+						var cmd = InitCmd(new ImportCommand(data)
+						{
+							Strict = false
+						});
+						cmd.Execute();
+						session.Flush();
+						transaction.Commit();
+					}
+					ws = session.Query<WaybillSettings>().FirstOrDefault(x => x.BelongsToAddress.Id == address.Id);
+					Assert.AreEqual("тестовый адрес доставки после ручного изменения", ws.Address);
+				}
+			}
 		}
 	}
 }
