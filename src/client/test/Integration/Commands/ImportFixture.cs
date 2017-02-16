d064f8bf07a947a6038efea40024a670665276c8
Бандеев Даша-Нима <d.bandeev@analit.net>
2017-02-16 15:02:40 +0800

    Ошибка #57225 Корректировка адресов доставки Test

--- src/client/test/Integration/Commands/ImportFixture.cs
+++ src/client/test/Integration/Commands/ImportFixture.cs
@@ -26,8 +26,7 @@ namespace AnalitF.Net.Client.Test.Integration.Commands
 			var data = new List<Tuple<string, string[]>> {
 				Tuple.Create(TempFile("Users.txt", "5\ttest\t"), new[] { "Id", "NonExistsColumn" })
 			};
-			var cmd = InitCmd(new ImportCommand(data)
-			{
+			var cmd = InitCmd(new ImportCommand(data){
 				Strict = false
 			});
 			cmd.Execute();
