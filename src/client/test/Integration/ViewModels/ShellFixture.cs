29d3c0a16e4ccde33f854e221fe902879c5b1864
Бандеев Даша-Нима <d.bandeev@analit.net>
2017-02-17 15:50:22 +0800

    Ошибка #56707 Программа не находит прежних путей для импорта документов Исправление теста

--- src/client/test/Integration/ViewModels/ShellFixture.cs
+++ src/client/test/Integration/ViewModels/ShellFixture.cs
@@ -124,9 +124,6 @@ namespace AnalitF.Net.Client.Test.Integration.ViewModels
 			settings.LastUpdate = null;
 			settings.UserName = null;
 			settings.Password = null;
-			settings.WaybillDir = settings.MapPath("Waybills");
-			settings.RejectDir = settings.MapPath("Rejects");
-			settings.ReportDir = settings.MapPath("Reports");
 			session.Flush();
 			shell.Reload();
 
@@ -157,9 +154,6 @@ namespace AnalitF.Net.Client.Test.Integration.ViewModels
 			settings.LastUpdate = DateTime.Now.AddDays(-1);
 			settings.UserName = "test";
 			settings.Password = "123";
-			settings.WaybillDir = settings.MapPath("Waybills");
-			settings.RejectDir = settings.MapPath("Rejects");
-			settings.ReportDir = settings.MapPath("Reports");
 			session.Flush();
 			shell.Reload();
 
