ebdba7d95135a7f789cadabe5b63b147302fb52b
Квасов Роман <r.kvasov@analit.net>
2017-02-21 15:38:08 +0300

    Исправил тесты

--- src/client/test/TestHelpers/BindingChecker.cs
+++ src/client/test/TestHelpers/BindingChecker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Disposables;
 using AnalitF.Net.Client.Test.Integration.Views;
 using Common.Tools;
@@ -13,6 +14,12 @@ namespace AnalitF.Net.Client.Test.TestHelpers
 			ViewSetup.BindingErrors.Clear();
 			return Disposable.Create(() => {
 				var errors = ViewSetup.BindingErrors.ToArray();
+				var ignored = new [] {
+					"System.Windows.Data Error: 4 : ",
+					"Cannot find source for binding with reference 'RelativeSource FindAncestor, AncestorType='System.Windows.Controls.ItemsControl', AncestorLevel='1''. BindingExpression:Path=VerticalContentAlignment; DataItem=null; target element is 'MenuItem' (Name=''); target property is 'VerticalContentAlignment' (type 'VerticalAlignment')",
+					"Cannot find source for binding with reference 'RelativeSource FindAncestor, AncestorType='System.Windows.Controls.ItemsControl', AncestorLevel='1''. BindingExpression:Path=HorizontalContentAlignment; DataItem=null; target element is 'MenuItem' (Name=''); target property is 'HorizontalContentAlignment' (type 'HorizontalAlignment')"
+				};
+				errors = errors.Except(ignored).ToArray();
 				if (errors.Length > 0) {
 					throw new Exception(errors.Implode(Environment.NewLine));
 				}
