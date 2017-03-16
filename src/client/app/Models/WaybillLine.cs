3697b31c93c2b7304573c5ef341afee661b863d4
Ивар Лекса <i.lexa@analit.net>
2017-03-16 14:52:00 +0300

    Ошибка #60661 Запретить добавлять накладные от поставщиков, работающих в нашей системе. исправлено добавление строки исправлена подсветка ячеек

--- src/client/app/Models/WaybillLine.cs
+++ src/client/app/Models/WaybillLine.cs
@@ -253,7 +253,7 @@ namespace AnalitF.Net.Client.Models
 		public virtual bool IsSupplierPriceMarkupInvalid => SupplierPriceMarkup > _maxSupplierMarkup;
 
 		[Style("Product", Description = "Название препарата: не указано")]
-		public virtual bool IsProductEmpty => CatalogId == null;
+		public virtual bool IsProductEmpty => CatalogId == null && string.IsNullOrWhiteSpace(Product);
 
 		[Style("SupplierCost", Description = "Цена поставщика с НДС: не указана")]
 		public virtual bool IsSupplierCostInvalid => (SupplierCost ?? 0) <= 0;
---
+++ src/client/app/Views/AddWaybillLineFromCatalog.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AnalitF.Net.Client.Views
+{
+	/// <summary>
+	/// Interaction logic for SelectFromCatalog.xaml
+	/// </summary>
+	public partial class AddWaybillLineFromCatalog : UserControl
+	{
+		public AddWaybillLineFromCatalog()
+		{
+			InitializeComponent();
+
+			IsVisibleChanged += AddWaybillLineFromCatalog_IsVisibleChanged;
+
+			Catalogs.GotFocus += ComboBox_GotFocus;
+			Producers.GotFocus += ComboBox_GotFocus;
+
+			SupplierCost_Value.GotFocus += TextBox_GotFocus;
+			Quantity_Value.GotFocus += TextBox_GotFocus;
+		}
+
+		private void ComboBox_GotFocus(object sender, RoutedEventArgs e)
+		{
+			(sender as ComboBox).IsDropDownOpen = true;
+		}
+
+		private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+		{
+			var textBox = sender as TextBox;
+			textBox.SelectionStart = 0;
+			textBox.SelectionLength = textBox.Text.Length;
+		}
+
+		private void AddWaybillLineFromCatalog_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if ((bool)e.NewValue == false)
+				return;
+			Dispatcher.BeginInvoke(DispatcherPriority.ContextIdle, new Action(delegate() { Catalogs.Focus(); }));
+		}
+	}
+}
