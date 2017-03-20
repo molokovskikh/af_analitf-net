5a3ccbe29c095872d439ef776a898c24dd14c410
Бандеев Даша-Нима <d.bandeev@analit.net>
2017-03-20 08:22:49 +0800

    Ошибка #59281 Ассортимент товара в системе учета должен формироваться из наименований нашего каталога .

--- migrations/2270_00_59281_IStockDoc.sql
+++ migrations/2270_00_59281_IStockDoc.sql
@@ -1,24 +1,24 @@
 use Inventory;
 alter table Inventory.Checks
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
 
 alter table Inventory.displacementdocs
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
   
 alter table Inventory.inventorydocs
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
   
 alter table Inventory.reassessmentdocs
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
   
 alter table Inventory.returndocs
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
     
 alter table Inventory.unpackingdocs
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
     
 alter table Inventory.writeoffdocs
-  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '0.00000';
+  add column `NumberDoc` VARCHAR(255) NOT NULL DEFAULT '';
   
 alter table Inventory.inventorylines  
   add column `DocId` INT(10) UNSIGNED NULL DEFAULT NULL;
