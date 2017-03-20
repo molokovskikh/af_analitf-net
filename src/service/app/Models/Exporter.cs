b7a33dea7b564ba96e5520a683efe6e2ceb13a21
Ивар Лекса <i.lexa@analit.net>
2017-03-20 18:22:59 +0300

    Исправил мердж

--- src/service/app/Models/Exporter.cs
+++ src/service/app/Models/Exporter.cs
@@ -1874,7 +1874,6 @@ select l.CheckId as ServerDocId,
 	l.VitallyImportant,
 	l.SupplyQuantity,
 	l.Exp,
-	l.PaymentByCard,
 	l.DocId
 from Inventory.CheckLines l
 	join Inventory.Checks c on c.Id = l.CheckId
